4d6d85a0b317c8986dae44dccdc2732de441df86
Diego Henrick <diegohenrick@Diegos-MacBook-Pro.local>
2025-10-26 20:30:03 -0300

    minimum version 16

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "MashiCoreKit",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v18),
+        .iOS(.v16),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
